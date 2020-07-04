754d0f25b561f86e14b20b6235d44d2382a81f9a
Josue Rivera <jzjosuerivera71@gmail.com>
2020-07-04 02:57:57 +0200

    fourth commit

--- main_script.py
+++ main_script.py
@@ -5,34 +5,14 @@ from p_wrangling import m_wrangling as wr
 from p_analysis import m_analysis as an 
 #from p_reporting import m_reporting as re 
 
-#def argument_parser():
-#    parser = argparse.ArgumentParser(description = 'Set chart type')
-#    parser.add_argument("-b", "--bar", help="Produce a barplot", action="store_true")
-#    parser.add_argument("-l", "--line", help="Produce a lineplot", action="store_true")
-#    args = parser.parse_args()
-#    return args
 
-#def main(some_args):
-#    data = mac.acquire()
-#    filtered = mwr.wrangle(data, year)
-#    results = man.analyze(filtered)
-#    fig = mre.plotting_function(results, title, arguments)
-#    mre.save_viz(fig, title)
-#    print('========================= Pipeline is complete. You may find the results in the folder ./data/results =========================')
 
-#if __name__ == '__main__':
-#    year = int(input('Enter the year: '))
-#    title = 'Top 10 Manufacturers by Fuel Efficiency ' + str(year)
-#    arguments = argument_parser()
-#    main(arguments)
-
-#Mi argument_parser
  
-def argument_parser():
-	parser = argparse.ArgumentParser(description = 'Which table you #want?')
-	parser.add_argument("-p","--path",type=str,help = 'specify table')
-	args = parser.parse_args()
-	return args
+#def argument_parser():
+#	parser = argparse.ArgumentParser(description = 'Which table you #want?')
+#	parser.add_argument("-p","--path",type=str,help = 'specify table')
+#	args = parser.parse_args()
+#	return args
 	
 def main(args):
 	df_2=ac.acquire('country_info')
@@ -42,15 +22,16 @@ def main(args):
 	merge_df=wr.Merge(df_2,df_3,df_4,Cl_df_4)
 	if args == 'by_coutry':
 		x=an.Analisis_1(merge_df)
-		print(np.array(x))
+		y=wr.Translator_1(x)
+		print(y)
 	elif args == 'by_jobs':
 		x=an.Analisis_2(merge_df)
-		print(np.array(x))
+		y=wr.Translator_1(x)
+		print(y)
 		
-#    print()
 
-#Options: {'poll_info','country_info','personal_info','career_info'}
 
 if __name__ == '__main__':
-	args = argument_parser() 
+	#args = argument_parser()
+	args = 'by_coutry' 
 	main(args)
--- p_wrangling/m_wrangling.py
+++ p_wrangling/m_wrangling.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+import requests
+from bs4 import BeautifulSoup
+#import re
 # wrangling functions
 
 def Cleaning_Dataframe_1(df_4):
@@ -44,3 +46,105 @@ def Merge(df_2,df_3,df_4,Cl_df_4):
     colnames = ['uuid','gender','country_code','normalized_job_code']
     new_df = pd.DataFrame(lst_lst, columns=colnames)
     return new_df
+
+
+
+grou=[['BE', 'Belgium'], ['EL', 'Greece'], ['LT', 'Lithuania'], ['PT', 'Portugal'], ['BG', 'Bulgaria'], ['ES', 'Spain'], ['LU', 'Luxembourg'], ['RO', 'Romania'], ['CZ', 'Czechia'], ['FR', 'France'], ['HU', 'Hungary'], ['SI', 'Slovenia'], ['DK', 'Denmark'], ['HR', 'Croatia'], ['MT', 'Malta'], ['SK', 'Slovakia'], ['DE', 'Germany'], ['IT', 'Italy'], ['NL', 'Netherlands'], ['FI', 'Finland'], ['EE', 'Estonia'], ['CY', 'Cyprus'], ['AT', 'Austria'], ['SE', 'Sweden'], ['IE', 'Ireland'], ['LV', 'Latvia'], ['PL', 'Poland'], ['UK', 'UnitedKingdom'], ['IS', 'Iceland'], ['NO', 'Norway'], ['LI', 'Liechtenstein'], ['CH', 'Switzerland'], ['ME', 'Montenegro'], ['MK', 'NorthMacedonia'], ['AL', 'Albania'], ['RS', 'Serbia'], ['TR', 'Turkey'], ['BA', 'BosniaandHerzegovina'], ['XK', 'Kosovo'], ['AM', 'Armenia'], ['BY', 'Belarus'], ['GE', 'Georgia'], ['AZ', 'Azerbaijan'], ['MD', 'Moldova'], ['UA', 'Ukraine'], ['DZ', 'Algeria'], ['LB', 'Lebanon'], ['SY', 'Syria'], ['EG', 'Egypt'], ['LY', 'Libya'], ['TN', 'Tunisia'], ['IL', 'Israel'], ['MA', 'Morocco'], ['JO', 'Jordan'], ['PS', 'Palestine'], ['RU', 'Russia'], ['AR', 'Argentina'], ['CN_X_HK', 'China'], ['MX', 'Mexico'], ['ZA', 'SouthAfrica'], ['AU', 'Australia'], ['HK', 'HongKong'], ['NG', 'Nigeria'], ['KR', 'SouthKorea'], ['BR', 'Brazil'], ['IN', 'India'], ['NZ', 'NewZealand'], ['TW', 'Taiwan'], ['CA', 'Canada'], ['JP', 'Japan'], ['SG', 'Singapore'], ['US', 'UnitedStates']]
+    
+    
+def Translator_1(analisis_df):
+	lst_country_code = list(analisis_df['country_code'])
+	# web-scraping
+	#url = 'https://ec.europa.eu/eurostat/statistics-explained/index.php/Glossary:Country_codes'
+	#html = requests.get(url).content
+	#soup = BeautifulSoup(html, 'lxml')
+	#table = soup.find('div',id = 'mw-content-text')
+	#rows = table.find_all('tr')
+	#rows_lst = [row for row in rows]
+	#rows_lst_2 = [str(i) for i in rows_lst]
+	#
+	#regex sub for cleaning
+	#rows_lst_3 = [re.sub('<tr>','',i) for i in rows_lst_2]
+	#rows_lst_4 = [re.sub('\n','',i) for i in rows_lst_3]
+	#rows_lst_5 = [re.sub('<td>','',i) for i in rows_lst_4]
+	#rows_lst_6 = [re.sub('</td>','',i) for i in rows_lst_5]
+	#rows_lst_7 = [re.sub('</tr>','',i) for i in rows_lst_6]
+	#rows_lst_8 = [re.sub('</a>','',i) for i in rows_lst_7]
+	#rows_lst_9 = [re.sub('\xa0','',i) for i in rows_lst_8]
+	#rows_lst_10 = [re.sub('<sup class="reference" id="cite_ref-3">','',i) for i in rows_lst_9]
+	#rows_lst_11 = [re.sub('<sup class="reference" id="cite_ref-2">','',i) for i in rows_lst_10]
+	#rows_lst_12 = [re.sub('<a href="#cite_note-2">','',i) for i in rows_lst_11]
+	#rows_lst_13 = [re.sub('</sup>','',i) for i in rows_lst_12]
+	#rows_lst_14 = [re.sub('<sup class="reference" id="cite_ref-1">','',i) for i in rows_lst_13]
+	#rows_lst_15 = [re.sub('<a href="#cite_note-1">','',i) for i in rows_lst_14]
+	#rows_lst_16 = [re.sub('[1]','',i) for i in rows_lst_15]
+	#rows_lst_17 =[re.sub('[2]','',i) for i in rows_lst_16]
+	#rows_lst_18 =[re.sub('[3]','',i) for i in rows_lst_17]
+	#rows_lst_19 =[re.sub('<a href="#cite_note-">','',i) for i in rows_lst_18]
+	#rows_lst_20 =[re.sub(' ','',i) for i in rows_lst_19]
+	#
+	#Split for cleaning
+	#split_lst_1 = [i.split('(') for i in rows_lst_20]
+	#low_dim_1 = [j for i in split_lst_1 for j in i]
+	#split_lst_2 = [i.split(')') for i in low_dim_1]
+	#low_dim_2 = [j for i in split_lst_2 for j in i]
+	#clening_1 = [i for i in low_dim_2 if i != '']
+	#clening_2 = [i for i in clening_1 if i != 'exceptHongKong']
+	#clening_3 = []
+	#
+	#for i in clening_2:
+	#    if i == 'Kosovo*[]':
+	#        clening_3.append('Kosovo')
+	#    else:
+	#        clening_3.append(i)
+	#
+	#clening_4 = []
+	#for i in clening_3:
+	#    if i == 'XK[]':
+	#        clening_4.append('XK')
+	#    else:
+	#        clening_4.append(i) 
+	#        
+	#clening_5 = []
+	#for i in clening_4:
+	#    if i == 'Palestine*[]':
+	#        clening_5.append('Palestine')
+	#    else:
+	#        clening_5.append(i)
+	#creo que me faleta un filtro para china
+	#Grouping:
+	#
+	#lst_code = [clening_5[i] for i in range(len(clening_5)) if (i % 2) != 0 ]
+	#lst_country = [clening_5[i] for i in range(len(clening_5)) if (i % 2) == 0]
+	#
+	#grou = [[] for i in range(int(len(clening_5)/2))] #initialization
+	#
+	#for i in range(len(grou)):
+	#    grou[i].append(lst_code[i])
+	#    grou[i].append(lst_country[i])
+	#    
+	#exchange
+	new_lst = []
+	for i in lst_country_code:
+		for j in range(len(grou)):
+			if i == grou[j][0]:
+				new_lst.append(grou[j][1])
+    	#
+    	# Prepare data for Dataframe
+	if len(list(analisis_df)) == 3:
+		lst_1_1= list(analisis_df['gender'])
+		lst_2_1= list(analisis_df['amount'])
+		lst_lst_1 = [[new_lst[i],lst_1_1[i],lst_2_1[i]] for i in range(len(new_lst))]
+		colnames_1 = ['country','gender','amount_of_people_by_country']
+		new_genders_df_1 = pd.DataFrame(lst_lst_1, columns=colnames_1)
+		return new_genders_df_1
+	elif len(list(analisis_df)) == 4:
+		lst_1_2= list(analisis_df['gender'])
+		lst_2_2= list(analisis_df['amount_of_people'])
+		lst_3_2= list(analisis_df['job_code'])
+		lst_lst_2 = [[new_lst[i],lst_1_2[i],lst_2_2[i],lst_3_2[i]] for i in range(len(new_lst))] 
+		colnames_2=['country','gender','amount_of_people_by_jobs','job_code']			
+		new_genders_df_2 = pd.DataFrame(lst_lst_2, columns=colnames_2)
+		return new_genders_df_2    
+
+           
